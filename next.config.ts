769b76048915aee09505413dab3c5a0b0e07e5de
Anim Bronyina <animbronyinaa@gmail.com>
2026-05-21 21:19:45 +0000

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next"
+import type { CapacitorConfig } from "@capacitor/cli"
 
-const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
+const config: CapacitorConfig = {
+  appId: "com.psst.app",
+  appName: "Psst",
+  webDir: "public",
+  server: {
+    url: "https://psst-kappa.vercel.app/",
+    cleartext: false,
   },
 }
 
-export default nextConfig
+export default config
